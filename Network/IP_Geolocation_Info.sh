2d3cc52543a0f3ca4ecb7e3e24338822a745901f
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-01-22 12:09:46 +0100

    Update IP_Geolocation_Info.sh
    added status code checked

--- Network/IP_Geolocation_Info.sh
+++ Network/IP_Geolocation_Info.sh
@@ -3,6 +3,14 @@
 # Send a curl request to ipinfo.io and capture the response
 response=$(curl -s ipinfo.io)
 
+# Check the HTTP status code
+status_code=$(curl -s -w "%{http_code}" ipinfo.io -o /dev/null)
+
+if [[ "$status_code" != "200" ]]; then
+  echo "Error: HTTP request failed with status code $status_code"
+  exit 1 # Exit with an error code
+fi
+
 # Extract the desired fields using jq
 ip=$(echo "$response" | jq -r '.ip')
 city=$(echo "$response" | jq -r '.city')
@@ -21,3 +29,5 @@ fi
 echo "$ip,$city,$region,$country,$loc,$org,$postal" >> ipinfo.csv
 
 echo "Data saved to ipinfo.csv"
+
+cat ipinfo.csv
